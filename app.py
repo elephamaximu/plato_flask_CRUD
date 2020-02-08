137b8ac566f45e559ce6e3222cbb4f848c1a8fbd
PLATO JUNG <elephamaximu@gmail.com>
2020-02-08 19:50:10 +0900

    유저 테이블 생성

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 import os
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-dbfile = os.path.join(basedir, 'db.sqlite')
+from models import db
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Test(db.Model):
-    __tablename__ = 'test_table'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-
-
-db.create_all()
 
 @app.route('/')
 def hello():
     return render_template('hello.html')
+
+if __name__ == '__main__':
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    dbfile = os.path.join(basedir, 'db.sqlite')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+
+    app.run(host='127.0.0.1', port=5000, debug=True)
